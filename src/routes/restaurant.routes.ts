5b6f6556fb719ebfa7c35665de2d7ffd0217ba5f
sharon884 <sharonsharon9048461583@gmail.com>
2025-11-22 15:50:13 +0530

    feat(api): add getRestaurantById function to fetch single restaurant details
    - Implemented new API function to retrieve restaurant details by ID
    - Added typed Axios request with proper response structure
    - Included error handling for failed or unreachable requests

--- src/routes/restaurant.routes.ts
+++ src/routes/restaurant.routes.ts
@@ -1,7 +1,7 @@
 // src/routes/restaurant.routes.ts
 
 import express from "express";
-import { getRestaurants, addRestaurant } from "../controllers/restaurant.controller";
+import { getRestaurants, addRestaurant,getRestaurantById } from "../controllers/restaurant.controller";
 import { validate } from '../middlewares/validateSchema'; // <-- New import
 import { createRestaurantSchema } from '../schemas/restaurant.schema'; // <-- New import
 
@@ -14,5 +14,7 @@ router.get("/", getRestaurants);
 // 1. Validation Middleware runs first
 // 2. Controller function runs next if validation passes
 router.post("/", validate(createRestaurantSchema), addRestaurant);
+
+router.get("/:id", getRestaurantById);
 
 export default router;
